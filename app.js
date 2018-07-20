1156558af754d5241f18635e38ee2913464df7eb
lotusfreak15 <lenny.claes@gmail.com>
2018-07-20 17:40:50 +0200

    Second commit

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const fs = require('fs');
 const session = require('express-session');
 
 let userData = fs.readFileSync('./userData.json');
+let tipData = fs.readFileSync('./tips.json');
+let opdrachtData = fs.readFileSync('./opdrachten.json');
 
 userData = JSON.parse(userData);
+tipData = JSON.parse(tipData);
+opdrachtData = JSON.parse(opdrachtData);
 
 let app = express();
 
@@ -70,17 +74,179 @@ app.get('/uitnodiging', (req, res) => {
 });
 
 app.get('/tips', (req, res) => {
+    let user = {
+        "_id": null,
+        "name": "Anonymous",
+        "character": "Unknown",
+        "costume": "Unknown",
+        "description": "Unknown"
+    }
+
+    if(req.session.user) {
+        user = req.session.user
+    }
+
     res.render('tips.ejs', {
         "logged_in": req.session.loggedIn,
-        "title": "Speelpleinweekend 3018 - Tips"
+        "title": "Speelpleinweekend 3018 - Tips",
+        "user": user,
+        "tips": tipData.tips
     });
 });
 
 app.get('/opdrachten', (req, res) => {
+    let user = {
+        "_id": null,
+        "name": "Anonymous",
+        "character": "Unknown",
+        "costume": "Unknown",
+        "description": "Unknown",
+        "is_admin": false
+    }
+
+    if(req.session.user) {
+        user = req.session.user
+    }
+
     res.render('opdrachten.ejs', {
         "logged_in": req.session.loggedIn,
-        "title": "Speelpleinweekend 3018 - Opdrachten"
+        "title": "Speelpleinweekend 3018 - Opdrachten",
+        "user": user,
+        "opdrachten": opdrachtData
     });
+});
+
+app.get('/tips/create', (req, res) => {
+    res.statusCode = 403;
+    res.send("You shouldn't be here!");
+});
+
+app.post('/tips/create', (req, res) => {
+    let tip = req.body.tip;
+
+    if(tip) {
+        tipData.tips.push({
+            "_id": tipData.tips.length,
+            "tip": tip
+        });
+        let dataToBeSaved = JSON.stringify(tipData);
+        fs.writeFile('./tips.json', dataToBeSaved, (err) => {
+            tipData = fs.readFileSync('./tips.json');
+            tipData = JSON.parse(tipData);
+            res.redirect('/tips');
+        });
+    } else {
+        res.redirect('/tips');
+    }
+});
+
+app.get('/tips/delete/:id', (req, res) => {
+    tipData.tips.splice(req.params.id, 1);
+    let dataToBeSaved = JSON.stringify(tipData);
+    fs.writeFile('./tips.json', dataToBeSaved, (err) => {
+        tipData = fs.readFileSync('./tips.json');
+        tipData = JSON.parse(tipData);
+        res.redirect('/tips');
+    });
+});
+
+app.get('/admin', (req, res) => {
+    let user = {
+        "_id": null,
+        "name": "Anonymous",
+        "character": "Unknown",
+        "costume": "Unknown",
+        "description": "Unknown",
+        "is_admin": false
+    }
+
+    if(req.session.user) {
+        user = req.session.user;
+        if(user.is_admin) {     
+            res.render('admin.ejs', {
+                "logged_in": req.session.logged_in,
+                "title": "Admin page",
+                "user": user,
+                "list": ""
+            });
+        }
+    } else {
+        res.redirect('/');
+    }
+})
+
+app.get('/admin/users', (req, res) => {
+    let user = {
+        "_id": null,
+        "name": "Anonymous",
+        "character": "Unknown",
+        "costume": "Unknown",
+        "description": "Unknown",
+        "is_admin": false
+    }
+
+    if(req.session.user) {
+        user = req.session.user;
+        if(user.is_admin) {     
+            res.render('admin.ejs', {
+                "logged_in": req.session.logged_in,
+                "title": "Admin page",
+                "user": user,
+                "list": "users",
+                "userslist": userData.users
+            });
+        }
+    } else {
+        res.redirect('/');
+    }
+});
+
+app.get('/admin/tips', (req, res) => {
+    let user = {
+        "_id": null,
+        "name": "Anonymous",
+        "character": "Unknown",
+        "costume": "Unknown",
+        "description": "Unknown",
+        "is_admin": false
+    }
+
+    if(req.session.user) {
+        user = req.session.user;
+        if(user.is_admin) {     
+            res.render('admin.ejs', {
+                "title": "Admin page",
+                "user": user,
+                "logged_in": req.session.logged_in
+            });
+        }
+    } else {
+        res.redirect('/');
+    }
+});
+
+app.get('/admin/opdrachten', (req, res) => {
+    let user = {
+        "_id": null,
+        "name": "Anonymous",
+        "character": "Unknown",
+        "costume": "Unknown",
+        "description": "Unknown",
+        "is_admin": false
+    }
+
+    if(req.session.user) {
+        user = req.session.user;
+        if(user.is_admin) {     
+            res.render('admin.ejs', {
+                "title": "Admin page",
+                "user": user,
+                "logged_in": req.session.logged_in
+            });
+        }
+    } else {
+        res.redirect('/');
+    }
 });
 
 app.get('/user/login', (req, res) => {
@@ -106,7 +272,8 @@ app.post('/user/login', (req, res) => {
                 "name": name + " " + userData.users[uid].l_name,
                 "character": userData.users[uid].character,
                 "costume": userData.users[uid].costume,
-                "description": userData.users[uid].description
+                "description": userData.users[uid].description,
+                "is_admin": userData.users[uid].is_admin
             };
             res.redirect('/uitnodiging');
         } else {
@@ -124,6 +291,32 @@ app.get('/user/logout', (req, res) => {
         req.session.destroy();
     }
     res.redirect('/');
+});
+
+app.post('/user/create', (req, res) => {
+    let fName = req.body.f_name;
+    let lName = req.body.l_name;
+    let costume = req.body.costume;
+    let character = req.body.character;
+    let description = req.body.description;
+    let pass = req.body.pass;
+    
+    userData.users.push({
+        "_id": userData.users.length,
+        "f_name": fName,
+        "l_name": lName,
+        "pass": pass,
+        "character": character,
+        "costume": costume,
+        "description": description,
+        "is_admin": false
+    });
+    let dataToBeSaved = JSON.stringify(userData);
+    fs.writeFile('./userData.json', dataToBeSaved, (err) => {
+        userData = fs.readFileSync('./userData.json');
+        userData = JSON.parse(userData);
+        res.redirect('/admin/users');
+    });
 });
 
 
